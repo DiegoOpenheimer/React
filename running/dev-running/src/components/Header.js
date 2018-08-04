fa3813fdca62772e15421ebef3b20b706c3fcff3
Diego Alves Openheimer <diegoop.pa@hotmail.com>
2018-08-04 12:46:46 -0300

    Criado componente header

--- running/dev-running/src/components/Header.js
+++ running/dev-running/src/components/Header.js
@@ -3,21 +3,13 @@ import logo from '../logo.svg';
 import { connect } from 'react-redux'
 import { doLogin } from '../reducers/actions/auth'
 
-class Header extends React.Component {
-
-    componentWillMount() {
-        console.log('PROPS: ', this.props)
-        this.props.doLogin()
-    }    
-
-    render() {
-        return(
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-        )
-    }
+const Header = props => {
+    return(
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      )
 }
 
 const mapStateToProps = state => {
